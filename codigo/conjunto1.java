aa492249362e09e35e93cdd7cb2c39349e991ba4
nicklessagus <nicklessagus@users.noreply.github.com>
2018-04-17 16:12:36 -0300

    Create conjunto1.java

---
+++ codigo/conjunto1.java
@@ -0,0 +1,73 @@
+```java
+
+package conjunto;
+
+import java.util.ArrayList;
+
+public class conjunto1<T> {
+	private ArrayList<T> elementos;
+	
+	conjunto1(){
+			elementos = new ArrayList<T>();
+		
+	} // Constructor1
+	Integer tamanio(){
+		return elementos.size();		
+	}
+	void Agregar(T elem) {
+		if (!elementos.contains(elem))
+			elementos.add(elem);	
+	}
+	T iesimo(Integer indice){// indice< tamaño()
+		return elementos.get(indice);
+		
+	} 
+	void union(conjunto1<T> c){ // union1: Destructiva
+		for(int i=0; i<c.tamanio();i++){
+			this.Agregar(c.iesimo(i));
+			//QUE PASA SI EN VEZ DE USAR EL AGREGAR LO PONIA DIRECTAMENTE EN EL ARRAYLIST ELEMS??
+		}
+	} 
+
+	conjunto1<T> union2(conjunto1<T> c){	// union2: No debe tener Aliasing!
+		conjunto1<T> res = new conjunto1<T>(); 
+
+		for(int i=0; i<this.tamanio();i++){
+			res.Agregar(this.iesimo(i));
+		}
+		for(int i=0; i<c.tamanio();i++){
+			res.Agregar(c.iesimo(i));
+		}
+
+		return res;
+		
+	}
+
+	void interseccion(conjunto1<T> c){ // interseccion 1: Destructiva
+		ArrayList<T> elemsNew = new ArrayList<T>();
+		
+		for(int i=0; i<c.tamanio();i++){
+			
+			if(elementos.contains(c.iesimo(i))){
+				elemsNew.add(c.iesimo(i));
+			}
+			this.elementos = elemsNew;	
+		}		
+		
+	} 
+	conjunto1<T> interseccion2(conjunto1<T> c){ // interseccion 2: No destrcutiva
+		conjunto1<T> res = new conjunto1<T>(); 
+
+		for(int i=0; i<c.tamanio();i++){
+			if(elementos.contains(c.iesimo(i))){
+				res.Agregar(c.iesimo(i));
+			}
+			
+		}
+		
+		return res;
+		
+	}
+}
+
+```
